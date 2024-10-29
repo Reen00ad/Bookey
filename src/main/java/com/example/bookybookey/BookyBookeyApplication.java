b527fe110a25bc81f338fe106b60b3f0906db845
Reen00ad <105815655+Reen00ad@users.noreply.github.com>
2024-10-29 14:39:20 +0300

    Add files via upload

---
+++ src/main/java/com/example/bookybookey/BookyBookeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookybookey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookyBookeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookyBookeyApplication.class, args);
+    }
+
+}
